7f22d12aa4d8d7d85f43c53b5c83e899290ef50d
Oleg Gromov <mail@oleggromov.com>
2017-08-18 22:49:01 +0300

    Adds port exposing and push script

---
+++ scripts/push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker tag hlc2017 stor.highloadcup.ru/travels/plain_falcon
+docker push stor.highloadcup.ru/travels/plain_falcon
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm $(docker ps -aq --filter name=hlc)
-docker run --name hlc -v /Users/gromych/home_projects/hlc2017/data:/tmp/data hlc2017
+docker run --name hlc -v /Users/gromych/home_projects/hlc2017/data:/tmp/data -p 3000:80 hlc2017
